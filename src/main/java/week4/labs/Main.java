ba823fd5e1fed4a160ad1e276c159288ca384094
ilhankDev <ilhank.official@gmail.com>
2023-12-26 20:57:59 +0100

    Initial commit.

--- src/main/java/week4/labs/Main.java
+++ src/main/java/week4/labs/Main.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        List<Person> people = new ArrayList<Person>();
+        List<Person> people = new ArrayList<>();
         people.add(new Teacher("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa", 1200));
         people.add(new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki"));
 
