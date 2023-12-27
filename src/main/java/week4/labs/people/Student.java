b113cb4d5b3a65bc0dbf95ff616d841510555019
ilhankDev <ilhank.official@gmail.com>
2023-12-27 12:52:55 +0100

    Initial commit.

--- src/main/java/week4/labs/people/Student.java
+++ src/main/java/week4/labs/people/Student.java
@@ -2,7 +2,6 @@ package week4.labs.people;
 
 public class Student extends Person {
     private static final int INITIAL_CREDITS = 0;
-
     private String studentId;
     private int credits;
 
@@ -17,7 +16,7 @@ public class Student extends Person {
     }
 
     public void study() {
-        credits += 5; // Assuming each study session adds 5 credits
+        credits += 5;
     }
 
     @Override
