5515e447c0775947646dbdff9c6f2337564cbe1f
ilhankDev <ilhank.official@gmail.com>
2024-01-05 11:42:52 +0100

    Added Week 6 - NightSky Package.

--- src/main/java/week6/nightsky/MainNightSky.java
+++ src/main/java/week6/nightsky/MainNightSky.java
@@ -1,4 +1,16 @@
 package week6.nightsky;
 
 public class MainNightSky {
+    public static void main(String[] args) {
+
+        NightSky nightSkyOne = new NightSky(0.1, 40, 10);
+        nightSkyOne.print();
+        System.out.println("Number of stars: " + nightSkyOne.starsInLastPrint());
+
+        NightSky nightSkyTwo = new NightSky(0.2, 15, 6);
+        nightSkyTwo.print();
+        System.out.println("Number of stars: " + nightSkyTwo.starsInLastPrint());
+
+    }
 }
+
